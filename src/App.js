0abf69b7e9bbe574c1b02be44a147783af220a6c
Alessandro Capobianco <keape@Mac-mini-di-Alessandro.local>
2025-04-19 18:49:34 +0200

    Fix case sensitivity in Navbar import

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './navbar';
 import Home from './Home';
-import Budget from './Budget';
-import Navbar from './Navbar';
 import Filtri from './Filtri';
-import { ThemeProvider } from './ThemeContext';
+import Entrate from './Entrate';
+import Login from './Login';
+import ProtectedRoute from './ProtectedRoute';
 
 function App() {
   return (
-    <ThemeProvider>
-      <Router>
-        <div className="theme-container min-h-screen">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/budget" element={<Budget />} />
-            <Route path="/filtri" element={<Filtri />} />
-          </Routes>
-        </div>
-      </Router>
-    </ThemeProvider>
+    <Router>
+      <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          } />
+          <Route path="/filtri" element={
+            <ProtectedRoute>
+              <Filtri />
+            </ProtectedRoute>
+          } />
+          <Route path="/budget-entrate" element={
+            <ProtectedRoute>
+              <Entrate />
+            </ProtectedRoute>
+          } />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
