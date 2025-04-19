69457a5f719416d9e1b0d19b150269d3a2cb92f8
Alessandro Capobianco <keape@Mac-mini-di-Alessandro.local>
2025-04-19 18:50:52 +0200

    Fix Navbar import case to match actual file name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Navbar from './navbar';
+import Navbar from './Navbar';
 import Home from './Home';
 import Filtri from './Filtri';
 import Entrate from './Entrate';
