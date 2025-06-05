7369987e670a24cd0c3c9b8d653077de63a53d69
keape <keape86@gmail.com>
2025-06-05 22:38:46 +0200

    Add password change and recovery functionality3

--- server/index.js
+++ server/index.js
@@ -49,6 +49,24 @@ app.use((req, res, next) => {
   next();
 });
 
+// Middleware per l'autenticazione JWT
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token di accesso richiesto' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Token non valido' });
+    }
+    req.user = user;
+    next();
+  });
+};
+
 // Connessione a MongoDB
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
