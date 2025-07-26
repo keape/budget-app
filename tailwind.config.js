05d46173d85beb7a463bc2db8c30f4bf82d8315b
keape <keape86@gmail.com>
2025-07-26 15:21:45 +0200

    Fix Tailwind dark mode configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
-  module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    darkMode: 'class', // Abilita dark mode basato sulla classe
     safelist: [
         'border-pink-400',
         'border-purple-400',
