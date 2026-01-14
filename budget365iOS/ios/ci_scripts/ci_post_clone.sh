4d2ee1ad706ccaf1b7c572ede33c50adfd1d4bf0
keape <keape86@gmail.com>
2026-01-14 18:46:26 +0100

    errori git

--- budget365iOS/ios/ci_scripts/ci_post_clone.sh
+++ budget365iOS/ios/ci_scripts/ci_post_clone.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -e
 
+# Homebrew is available in Xcode Cloud. Ensure Node.js is installed.
+if ! command -v npm &> /dev/null
+then
+    echo "npm not found, installing node via Homebrew..."
+    brew install node
+fi
+
 # Move to the React Native project root
 cd $CI_PRIMARY_REPOSITORY_PATH/budget365iOS
 
